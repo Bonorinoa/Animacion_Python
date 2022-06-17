ae484c375b09762f73d80b286def67cadf0cc85a
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2022-06-17 19:55:43 -0300

    Added econ graphs and functions

--- Manim/manimEcon.py
+++ Manim/manimEcon.py
@@ -59,3 +59,91 @@ class indifferenceCurve(Scene):
 
 
         self.add(ax, title, curves, labels)
+
+class MonoToPoly(Scene):
+    def construct(self):
+
+        eq1 = MathTex(r"f_1(x) = 3x^4").shift(4*LEFT)
+        eq2 = MathTex(r"f_2(x) = -x^7")
+        eq3 = MathTex(r"f_3(x) = -10x^2").shift(4*RIGHT)
+        eqs = VGroup(eq1, eq2, eq3)
+
+        poly = MathTex(r"h(x) = 3x^4 - x^7 - 10x^2")
+
+        text1 = Text("Monomials").shift(2*UP)
+        text2 = Text("Polynomial").shift(2*UP)
+
+        self.add(text1)
+        self.add(eqs)
+        self.wait(2)
+        self.play(ReplacementTransform(text1, text2))
+        self.play(ReplacementTransform(eqs, poly))
+        self.wait(3)
+
+class DifferentFunctions(Scene):
+    def construct(self):
+
+        rat = MathTex(r"y_1 = \frac{x^2 + 1}{x - 1}").shift(2*LEFT)
+        rat2 = MathTex(r"y_2 = \frac{x^5 + 7x}{5}").shift(2*RIGHT)
+
+        expn = MathTex(r"y_1 = 10^x").shift(LEFT*2)
+        expn2 = MathTex(r"y_2 = \frac{3x}{2} + e^x").shift(2*RIGHT)
+
+        trig = MathTex(r"y_1 = sin(x)").shift(LEFT*2)
+        trig2 = MathTex(r"y_2 = cos(x) - sin(2x)").shift(2*RIGHT)
+        
+        rats = VGroup(rat, rat2)
+        expns = VGroup(expn, expn2)
+        trigs = VGroup(trig, trig2)
+
+        text1 = Text("Rational functions").shift(2*UP)
+        text2 = Text("Exponential functions").shift(2*UP)
+        text3 = Text("Trigonometric functions").shift(2*UP)
+
+        self.add(text1)
+        self.add(rats)
+        self.wait(2)
+        self.play(ReplacementTransform(text1, text2))
+        self.play(ReplacementTransform(rats, expns))
+        self.wait(2)
+        self.play(ReplacementTransform(text2, text3))
+        self.play(ReplacementTransform(expns, trigs))
+        self.wait(3)
+        
+class LocalMinMax(Scene):
+    def construct(self):
+
+        ax = Axes().add_coordinates()
+
+        func = ax.plot(lambda x: (x-2)**2 + 1, color=WHITE)
+        dot = Dot(ax.coords_to_point(2, 1), color=RED)
+        lines = ax.get_lines_to_point(ax.c2p(2,1))
+
+        coords = np.around(ax.point_to_coords(dot.get_right()))
+        label = (
+            Matrix([[coords[0]], [coords[1]]]).scale(0.35).next_to(dot, UP)
+        )
+
+        parab = VGroup(ax, func, dot, lines, label)
+
+        self.add(parab)
+        self.wait(1)
+        #self.play(VGroup(label).animate(run_time=2))
+
+        ## transform to downward sloping parabola + local max
+
+        ax2 = Axes().add_coordinates()
+
+        func2 = ax2.plot(lambda x: -( (x-2)**2 ) + 1, color=WHITE)
+        dot2 = Dot(ax2.coords_to_point(2, 1), color=RED)
+        lines2 = ax2.get_lines_to_point(ax2.c2p(2,1))
+
+        coords2 = np.around(ax2.point_to_coords(dot2.get_right()))
+        label2 = (
+            Matrix([[coords2[0]], [coords2[1]]]).scale(0.35).next_to(dot2, UP)
+        )
+
+        parab2 = VGroup(ax2, func2, dot2, lines2, label2)
+
+        self.play(ReplacementTransform(parab, parab2))
+        self.wait(2)
