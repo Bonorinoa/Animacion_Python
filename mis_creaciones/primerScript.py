68a03545e40533f25f6439aae8c4c9aa34e59aec
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2022-07-29 09:13:49 -0300

    agregagmos primerScript.py

---
+++ mis_creaciones/primerScript.py
@@ -0,0 +1,22 @@
+from manim import *
+
+class PrimerScript(Scene):
+    def construct(self):
+        
+        cuadrado = Square()
+        circuloAzul = Circle(color=BLUE).shift(LEFT*2)
+        
+        self.add(cuadrado)
+        self.play(Create(circuloAzul))
+        
+        self.wait()
+        
+        self.play(circuloAzul.animate.set_color(GREEN), Rotate(cuadrado))
+        
+        self.wait()
+        
+        self.play(FadeOut(circuloAzul))
+        self.play(ReplacementTransform(cuadrado, Triangle()))
+        
+        self.wait(2)
+        
